e1ed6b3fb3c913bfb98527ddca6f1a9ff4176f82
Cloudy-Skies <mr.armah.19@gmail.com>
2023-09-12 14:37:04 -0700

    task 2 done

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - No entry point
+ *
+ * Return: Always Success (0).
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - No entry point
+ *
+ * Return: On success nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
