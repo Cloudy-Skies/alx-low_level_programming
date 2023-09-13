e4bb9e591dfe6e14454c87f90c376e65c13302e1
Cloudy-Skies <mr.armah.19@gmail.com>
2023-09-12 18:51:42 -0700

    task 12 done fr

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,35 +8,44 @@
  */
 void print_times_table(int n)
 {
-	int i, j, product;
+	int x, y, z;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 14)
 	{
-		for (j = 0; j <= n; j++)
+		for (x = 0; x <= n; x++)
 		{
-			product = i * j;
-			if (product <= 9)
+			for (y = 0; y <= n; y++)
 			{
-				_putchar(product + '0');
-				if (j < n)
+				z = x * y;
+				if (z > 99)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-			}
-			else
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-				if (j < n)
+				else if (z > 9)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
 				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
