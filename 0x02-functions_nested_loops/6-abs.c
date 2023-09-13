58c6eb9d94c89387352442b17f1a8b24cd8c0073
Cloudy-Skies <mr.armah.19@gmail.com>
2023-09-12 17:07:43 -0700

    task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * _abs - check main
+ * @r: An integer input
+ * Description: A function that computes the absolute value of an int.
+ * Return: Prints the absolute value of r
+ */
+int _abs(int r)
+{
+	return (abs(r));
+}
